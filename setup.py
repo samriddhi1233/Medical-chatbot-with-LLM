294ca164cd69584cfb9b17e75ab5e4ca6146f13e
Samriddhi Karki <samriddhikarki745@gmail.com>
2025-11-19 21:24:34 +0545

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Samriddhi Karki",
+    author_email="samriddhikarki745@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
